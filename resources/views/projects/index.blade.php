a2d1091b50181c35b3910ec726f00e36c255f1bc
Clebson Moura <clebson.sh@gmail.com>
2023-05-03 13:54:48 -0300

    Let's Begin With a Test

---
+++ resources/views/projects/index.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Birdboard</title>
+</head>
+
+<body>
+    <h1>Birdboard</h1>
+
+    <ul>
+        @foreach ($projects as $project)
+            <li>{{ $project->title }}</li>
+        @endforeach
+    </ul>
+</body>
+
+</html>
